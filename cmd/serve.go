bff0699aafd4ebf4ed79cc66bad0ff8e2a8dd9d7
Jens Neuse <jens.neuse@gmx.de>
2020-01-16 11:48:51 +0100

    update to newest version of graphql go tools and use abstractlogger

--- cmd/serve.go
+++ cmd/serve.go
@@ -4,13 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gobwas/ws"
+	log "github.com/jensneuse/abstractlogger"
 	"github.com/jensneuse/graphql-go-tools/pkg/execution"
 	gqlHTTP "github.com/jensneuse/graphql-go-tools/pkg/http"
 	"github.com/jensneuse/graphql-go-tools/pkg/playground"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"strings"
 	"time"
@@ -40,7 +40,7 @@ func init() {
 	_ = viper.BindPFlag("loggerConfig", rootCmd.PersistentFlags().Lookup("loggerConfig"))
 }
 
-func logger() *zap.Logger {
+func logger() log.Logger {
 	configData, err := ioutil.ReadFile(loggerConfigFile)
 	if err != nil {
 		panic(err)
@@ -53,7 +53,7 @@ func logger() *zap.Logger {
 	if err != nil {
 		panic(err)
 	}
-	return logger
+	return log.NewZapLogger(logger,log.DebugLevel)
 }
 
 func startServer() {
@@ -65,51 +65,51 @@ func startServer() {
 	graphqlEndpoint := "/graphql"
 	schemaData, err := ioutil.ReadFile(schemaFile)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal("reading schema",log.Error(err))
 	}
 	handler, err := execution.NewHandler(schemaData, logger)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal("creating handler",log.Error(err))
 	}
 
 	upgrader := &ws.DefaultHTTPUpgrader
 	upgrader.Header = http.Header{}
 	upgrader.Header.Add("Sec-Websocket-Protocol", "graphql-ws")
 	mux.HandleFunc("/time", func(writer http.ResponseWriter, request *http.Request) {
-		_,err := writer.Write(fakeResponse())
+		_, err := writer.Write(fakeResponse())
 		if err != nil {
 			logger.Error("time_write_err",
-				zap.Error(err),
+				log.Error(err),
 			)
 		}
 	})
-	mux.Handle(graphqlEndpoint, gqlHTTP.NewGraphqlHTTPHandlerFunc(handler,logger,upgrader))
+	mux.Handle(graphqlEndpoint, gqlHTTP.NewGraphqlHTTPHandlerFunc(handler, logger, upgrader))
 	playgroundURLPrefix := "/playground"
 	playgroundURL := ""
 	err = playground.ConfigureHandlers(mux, playground.Config{
-		URLPrefix:       playgroundURLPrefix,
-		PlaygroundURL:   playgroundURL,
-		GraphqlEndpoint: graphqlEndpoint,
-		GraphQLSubscriptionEndpoint:graphqlEndpoint,
+		URLPrefix:                   playgroundURLPrefix,
+		PlaygroundURL:               playgroundURL,
+		GraphqlEndpoint:             graphqlEndpoint,
+		GraphQLSubscriptionEndpoint: graphqlEndpoint,
 	})
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal("configure handlers",log.Error(err))
 		return
 	}
 	addr := "0.0.0.0:9111"
 	logger.Info("Listening",
-		zap.String("add", addr),
+		log.String("add", addr),
 	)
-	fmt.Printf("Access Playground on: http://%s%s%s\n",prettyAddr(addr) , playgroundURLPrefix, playgroundURL)
+	fmt.Printf("Access Playground on: http://%s%s%s\n", prettyAddr(addr), playgroundURLPrefix, playgroundURL)
 	logger.Fatal("failed listening",
-		zap.Error(http.ListenAndServe(addr, mux)),
+		log.Error(http.ListenAndServe(addr, mux)),
 	)
 }
 
-func fakeResponse () []byte {
-	return []byte(`{"week_number":45,"utc_offset":"+01:00","utc_datetime":"2019-11-07T14:02:02.475928+00:00","unixtime":1573135322,"timezone":"Europe/Berlin","raw_offset":3600,"dst_until":null,"dst_offset":0,"dst_from":null,"dst":false,"day_of_year":311,"day_of_week":4,"datetime":"`+ time.Now().String() +`","client_ip":"92.216.144.100","abbreviation":"CET"}`)
+func fakeResponse() []byte {
+	return []byte(`{"week_number":45,"utc_offset":"+01:00","utc_datetime":"2019-11-07T14:02:02.475928+00:00","unixtime":1573135322,"timezone":"Europe/Berlin","raw_offset":3600,"dst_until":null,"dst_offset":0,"dst_from":null,"dst":false,"day_of_year":311,"day_of_week":4,"datetime":"` + time.Now().String() + `","client_ip":"92.216.144.100","abbreviation":"CET"}`)
 }
 
 func prettyAddr(addr string) string {
-	return strings.Replace(addr,"0.0.0.0","localhost",-1)
+	return strings.Replace(addr, "0.0.0.0", "localhost", -1)
 }
--- go.mod
+++ go.mod
@@ -7,7 +7,8 @@ require (
 	github.com/eclipse/paho.mqtt.golang v1.2.0
 	github.com/gobuffalo/envy v1.7.1 // indirect
 	github.com/gobwas/ws v1.0.2
-	github.com/jensneuse/graphql-go-tools v1.8.3-0.20200110085204-33547f7335d4
+	github.com/jensneuse/abstractlogger v0.0.4
+	github.com/jensneuse/graphql-go-tools v1.8.3-0.20200115132524-2cb19fec47c7
 	github.com/magiconair/properties v1.8.1 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/nats-io/nats.go v1.9.1
@@ -17,7 +18,6 @@ require (
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.4.0
-	go.uber.org/zap v1.11.0
-	golang.org/x/sys v0.0.0-20191027211539-f8518d3b3627 // indirect
+	go.uber.org/zap v1.13.0
 	golang.org/x/text v0.3.2 // indirect
 )
